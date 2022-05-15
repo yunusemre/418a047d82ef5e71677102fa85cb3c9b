8a0bc34058b7b0c26f0202cf115d307eb9367cf6
Yunus Emre TATAR <emre.tatar@sestek.com>
2022-05-15 22:27:38 +0300

    netlify error fixed

--- src/components/Thumbs/Thumbs.tsx
+++ src/components/Thumbs/Thumbs.tsx
@@ -5,23 +5,19 @@ import './Thumbs.scss';
 const Thumbs = ({ variants, images, change }: any) => {
   return (
     <div className="product-photo-thumbs">
-      {images.map((img: Image) => {
-        if (
-          img.variant_ids.length > 0 &&
-          variants.map((vrnt: any) => img.variant_ids.includes(vrnt.id))
-        ) {
-          return (
-            <a
-              className={`product-photo-thumbs__item ${classNames({
-                big: img.width > img.height,
-              })}`}
-              key={img.id}
-              onClick={() => change(img.src)}>
-              <img className="c-pointer item-image" src={img.src} alt={img.alt} />
-            </a>
-          );
-        }
-      })}
+      {images.map((img: Image) =>
+        img.variant_ids.length > 0 &&
+        variants.map((vrnt: any) => img.variant_ids.includes(vrnt.id)) ? (
+          <a
+            className={`product-photo-thumbs__item ${classNames({
+              big: img.width > img.height,
+            })}`}
+            key={img.id}
+            onClick={() => change(img.src)}>
+            <img className="c-pointer item-image" src={img.src} alt={img.alt} />
+          </a>
+        ) : null
+      )}
     </div>
   );
 };
